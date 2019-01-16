bf48ca89f0453cde64a876d366e387e0ad8af144
Noah-Gilga <843446939@qq.com>
2019-01-16 11:22:52 +0800

    first update

--- js/open360.js
+++ js/open360.js
@@ -1,7 +1,7 @@
 	var handler360 = new Cesium.ScreenSpaceEventHandler(viewer.scene.canvas);
     handler360.setInputAction(function (movement) {
     var pick = viewer.scene.pick(movement.position);
-    if (Cesium.defined(pick) && (pick.id)) {//(pick.id.id === '360-1')) {
+    if (Cesium.defined(pick) && (pick.id.id === '360-1')) {//(pick.id.id === '360-1')) {
 		//打开iframe窗口
 		
 		var iframeWin = document.getElementById('iframe-360-1');
